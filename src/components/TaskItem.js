c1c7e440701b37cc7a70c7fe6bc9ae9a92b9507c
Brandon <brandonpretelt93@gmail.com>
2022-04-04 02:20:03 -0600

    :fire: removed comments

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -16,33 +16,3 @@ const TaskItem = ({todoItems}) => {
 
 export default TaskItem;
 
-/*  {edit ? (<div>'hi' </div>) :  <div>{item}</div>} */
-/*   <EditButton todoItems={todoItems}/> */
-
-/* 
-
-    <>
-            
-            {editMode ? 
-            <input onDoubleClick={(e) => {
-                enterEdit()
-                dispatch({type: 'EDIT_TODO', taskName: e.target.value})
-            }} type="text" placeholder={todoItems.taskName}  /> : 
-                <div onDoubleClick={()=>enterEdit()}>{todoItems.taskName}</div>}
-                <DeleteButton todoItems={todoItems} />
-           
-        </>
-
-*/
-
-/* 
-
-    const { dispatch } = useContext(TodoContext);
-    
-    const [editMode, setEditMode] = useState(false);
-    const enterEdit = () => {
-        setEditMode((editMode) => !editMode);
-    };
-
-
-*/
